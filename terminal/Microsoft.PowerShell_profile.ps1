eed1d8bd329ff0e8046a84385316da08cb0b061c
ARz <120076142+ItzARz@users.noreply.github.com>
2025-08-19 03:55:35 -0400

    Update Microsoft.PowerShell_profile.ps1

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh init pwsh --config 'C:\Users\Albert Rojas\AppData\Local\Programs\oh-my-posh\themes\arz.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\YOUR USER\AppData\Local\Programs\oh-my-posh\themes\arz.omp.json' | Invoke-Expression
 Import-Module -Name Terminal-Icons
